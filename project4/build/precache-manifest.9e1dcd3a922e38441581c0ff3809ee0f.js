ea1867afb7a1ad272b745798a0ac038f72c543b6
ximul61 <hasaanshimul@gmail.com>
2020-02-14 18:49:41 +0600

    Passing Value Through 2 class Based Component

---
+++ project4/build/precache-manifest.9e1dcd3a922e38441581c0ff3809ee0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7ef54b3a12add46150a43b4466d21c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e123f0286f3b09070dd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d8f8a9a2fcab8190030",
+    "url": "/static/js/2.c2d03615.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c2d03615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e123f0286f3b09070dd",
+    "url": "/static/js/main.18eac752.chunk.js"
+  },
+  {
+    "revision": "34f728aac8b33858bb21",
+    "url": "/static/js/runtime-main.5d50cbcd.js"
+  }
+]);
